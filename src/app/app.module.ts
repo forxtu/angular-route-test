9eecc18a916167023d38201765b5ec08c85f55b9
forxtu <forxtu@gmail.com>
2017-08-04 12:25:15 +0200

    feat(components, module): add 2 components, add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
